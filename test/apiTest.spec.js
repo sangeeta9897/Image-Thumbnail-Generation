886beef6f4fae1f95f667bac109387bebd8d89ff
Sangeeta Sharma <1998sangeetasharma@gmail.com>
2020-04-18 15:31:32 +0530

    change in test file

--- test/apiTest.spec.js
+++ test/apiTest.spec.js
@@ -1,27 +1,17 @@
-// import chai from 'chai'
-// // import chaiHttp from 'chai-http'
-// // import server from '../index'
-
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('../dist/index')
 var should = chai.should()
 chai.use(chaiHttp)
 
-// chai.request(server)
-//     .post('/signIn')
-//     .end((err, res) => {
-//         (res).should.have.status(200)
-//             (res.body).should.be.a('object')
-//         done();
-//     })
-
 const assert = require('assert')
 describe('Sign in test', () => {
   it('should throw error if token not generated successfully', () => {
     chai.request(server)
       .post('/signIn')
-      .end((err, res) => {
+      .send({'username': 'sangeeta'})
+      .send({'password':'sang'})
+      .end((_err, res) => {
         try {
           res.should.have.property('status', 200);
           (res.body).should.be.a('object')
@@ -36,9 +26,9 @@ describe('Image thumbnail test', () => {
   it('should throw error if token not generated successfully', () => {
     chai.request(server)
       .post('/getThumbnailImage')
-      .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODY5NTkzNzIsImV4cCI6MTU4NzA0NTc3Mn0.k-mr2mWzrCKDV2y4ZFKPU7PU1gUDIpwTPPkHUicW4yQ')
+      .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNhbmdlZXRhIiwiaWF0IjoxNTg3MjAzNTAyLCJleHAiOjE1ODcyODk5MDJ9.TONXbKziEH-9PZ1DcigGlsX5AwxtFz37yqudcTbmXi4')
       .set('Content-Type', 'application/json')
-    // .send({ "url": "https://images.unsplash.com/photo-1559628129-67cf63b72248?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" })
+     .send({"url": "https://images.unsplash.com/photo-1559628129-67cf63b72248?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" })
       .end((err, res) => {
         res.should.have.property('status', 200)
       })
